a8e614c8f72f6e4f0f16e323740817e34db0f676
Alex223124 <sologoubalex@gmail.com>
2019-09-01 17:21:41 +0200

    comments removed

--- db/migrate/20190831215959_create_areas.rb
+++ db/migrate/20190831215959_create_areas.rb
@@ -1,9 +1,7 @@
 class CreateAreas < ActiveRecord::Migration[5.2]
   def change
     create_table :areas do |t|
-      # t.st_polygon :polygon, :geometry => true, :srid => 3857
       t.st_polygon :polygon, :geometry => true, :srid => 4326
-      # 4326
       t.timestamps
     end
   end
