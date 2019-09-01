b064e1744e0dbc6e454bbd354d6107209b2a8ac4
Alex223124 <sologoubalex@gmail.com>
2019-09-01 17:21:11 +0200

    schema changed, srid for areas.polygon changed from 3857 to 4326

--- db/migrate/20190831215959_create_areas.rb
+++ db/migrate/20190831215959_create_areas.rb
@@ -1,7 +1,9 @@
 class CreateAreas < ActiveRecord::Migration[5.2]
   def change
     create_table :areas do |t|
-      t.st_polygon :polygon, :geometry => true, :srid => 3857
+      # t.st_polygon :polygon, :geometry => true, :srid => 3857
+      t.st_polygon :polygon, :geometry => true, :srid => 4326
+      # 4326
       t.timestamps
     end
   end
