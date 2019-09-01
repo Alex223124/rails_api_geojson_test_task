8ede71757ffd6126a20645188cf574622730483e
Alex223124 <sologoubalex@gmail.com>
2019-09-01 03:33:57 +0200

    seeds file updated (now we can load areas from json before starting application)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+path_to_given_areas = "#{Rails.root}/db/seeds/given_areas.json"
+Services::GeoJsonImport.new(path_to_given_areas).run
