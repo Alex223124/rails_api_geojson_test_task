c06af4879f0910acd38cf4d3ea5fe437a125841e
Alex223124 <sologoubalex@gmail.com>
2019-09-04 23:58:05 +0200

    logic of after_commit hook changed, scopes added

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,8 +1,31 @@
 class Area < ApplicationRecord
 
-  geocoded_by :name
+  has_many :points
 
-  after_commit :import_coordinates, if: -> { latitude.nil? && longitude.nil? }
+  scope :given_areas, -> { where('is_given = true') }
+  scope :areas_contains_points, -> (longitude, latitude) {
+    where("ST_Intersects(ST_GeomFromText('POINT(? ?)', 4326), areas.polygon)",longitude, latitude)
+  }
+
+  after_commit :import_coordinates, if: -> { is_given.nil? &&
+                                            (self.name.present? && self.points.blank?) }
+
+  def self.polygons_contains_point?(basic_scope = "all", long, lat)
+    case basic_scope
+    when "all"
+      areas_contains_points(long, lat).present?
+    when "given_areas"
+      given_areas.areas_contains_points(long, lat).present?
+    end
+  end
+
+  def coordinates
+    if polygon.present?
+      polygon.coordinates
+    elsif points.present?
+      points.pluck(:latitude, :longitude)
+    end
+  end
 
   private
 
