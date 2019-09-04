7121f06b411bc4c3fd49df1d90d8705a03f6b783
Alex223124 <sologoubalex@gmail.com>
2019-09-04 23:56:54 +0200

    whole worker rewritten

--- app/workers/import_coordinates_worker.rb
+++ app/workers/import_coordinates_worker.rb
@@ -3,10 +3,35 @@ class ImportCoordinatesWorker
   sidekiq_options retry: false
 
   def perform(id)
-    area = Area.find(id)
-    area.geocode
-    raise Exception, "Something went wrong" if area.latitude.nil? && area.logitude.nil?
-    area.save!
+    area(id)
+    coordinates
+    @area.points.build(latitude: @coordinates.first, longitude: @coordinates.second)
+    in_given
+    @area.save!
+  rescue StandardError => error
+    Rails.logger.info "Error in Services::GeoJsonImport: #{error}"
+  end
+
+  private
+
+  def coordinates
+    @coordinates ||= Geocoder.search(@area.name).first.coordinates
+  end
+
+  def area(id)
+    @area ||= Area.find(id)
+  end
+
+  def in_given
+    if inside_given_locations?
+      @area.in_given = true
+    else
+      @area.in_given = false
+    end
+  end
+
+  def inside_given_locations?
+    Area.polygons_contains_point?("given_areas", @coordinates.second, @coordinates.first)
   end
 
 end
